b7fade41f364800ceea83b83d28402dce1bd491b
Reptartheman <117610334+Reptartheman@users.noreply.github.com>
2023-05-03 21:27:10 -0400

    worked on the header and navbar real hard

---
+++ client/src/components/ProjectContainer.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import Header from "./Header/header";
+
+
+export default function ProjectContainer() {
+
+    return (
+        <>
+        <div>
+        <Header />
+        </div>
+        
+        </>
+    )
+
+}
