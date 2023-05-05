bbefc134eed3a6a99bf884dbb9797914e8db027e
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-05-04 20:52:37 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,39 @@
-import React from 'react' 
+import React, { useState } from 'react';
 import './App.css';
-import ProjectContainer from './components/ProjectContainer';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Homepage from './pages/Homepage';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Profile from './pages/Profile';
 
+function App() {
+  const [currentPage, setCurrentPage] = useState('Homepage');
 
-const App = () => <ProjectContainer />
+  const renderPage = () => {
+    if (currentPage === 'Homepage') {
+      return <Homepage />;
+    }
+    if (currentPage === 'Login') {
+      return <Login />;
+    }
+    if (currentPage === 'Signup') {
+      return <Signup />;
+    }
+    if (currentPage === 'Profile') {
+      return <Profile />;
+    }
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <main>
+      <Header currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
+      <Footer />
+    </main>
+  );
+}
 
 export default App;
