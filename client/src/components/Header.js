12845637a1199bb274939c8aaa52666f3b1c6bd7
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-05-04 20:52:42 -0400

    Update Header.js

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,18 +1,32 @@
-import React from "react";
-import "../../App.css";
+import React from 'react';
+import '../App.css';
 
-export default function Header() {
+export default function Header({ currentPage, handlePageChange }) {
   return (
     <header className="header">
       <h1> Doggy Dojo </h1>
       <h2> Helping Your Dog Reach Their Pet-tential! </h2>
-      <nav className="button-container" role="group" aria-label="Outlined Buttons">
-        <button type="button" className="navButtons">Sample</button>
-        <button type="button" className="navButtons">
-          Sample
+      <nav
+        className="button-container"
+        role="group"
+        aria-label="Outlined Buttons"
+      >
+        <button
+          type="button"
+          className="navButtons"
+          onClick={() => handlePageChange('Homepage')}
+        >
+          Home
         </button>
-        <button type="button" className="navButtons">Sample</button>
-        <button type="button"className="navButtons">Sample</button>
+        <button type="button" className="navButtons">
+          Login
+        </button>
+        <button type="button" className="navButtons">
+          Signup
+        </button>
+        <button type="button" className="navButtons">
+          Profile
+        </button>
       </nav>
     </header>
   );
