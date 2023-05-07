0980a6d46fd256c99d32f20a2f4902b37b33343a
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-05-07 17:15:40 -0400

    Capitalize Footer.js and autoformat code

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -1,17 +1,19 @@
-import React from 'react' 
-import "../App.css";
-import GitHubIcon from "../assets/github-sign.png";
-export default function Footer () {
-    return (
-        <footer className="pagefooter">
-        <div>
-    <a href="https://github.com/Reptartheman/DoggyDojo" target={"_blank"}>
-      <img src={GitHubIcon} alt="GitHubLink" width="50" height="50"></img>
-    </a>
-        </div>
-        <p className="footerInfo">
-        Check out the repo, DAWG! It's ironic because it's a cat icon.<br />
+import React from 'react';
+import '../App.css';
+import GitHubIcon from '../assets/github-sign.png';
+
+export default function Footer() {
+  return (
+    <footer className="pagefooter">
+      <div>
+        <a href="https://github.com/Reptartheman/DoggyDojo" target={'_blank'}>
+          <img src={GitHubIcon} alt="GitHubLink" width="50" height="50"></img>
+        </a>
+      </div>
+      <p className="footerInfo">
+        Check out the repo, DAWG! It's ironic because it's a cat icon.
+        <br />
       </p>
     </footer>
-    )
+  );
 }
