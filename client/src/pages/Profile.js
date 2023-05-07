cab668305b4018b6888e4b2fe3bf58a916c22614
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-05-07 17:16:12 -0400

    Comment out code so that React works (for now)

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,62 +1,68 @@
-import React from 'react';
-import { Navigate, useParams } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
+import React, { useState } from 'react';
 
-import Checklist from '../components/Checklist';
-import Note from '../components/Note';
+export default function Login() {
+  return <h1>Profile Test</h1>;
+}
 
-import { QUERY_USER, QUERY_ME } from '../utils/queries';
+// import React from 'react';
+// import { Navigate, useParams } from 'react-router-dom';
+// import { useQuery } from '@apollo/client';
 
-import Auth from '../utils/auth';
+// import Checklist from '../components/Checklist';
+// import Note from '../components/Note';
 
-const Profile = () => {
-  const { username: userParam } = useParams();
+// import { QUERY_USER, QUERY_ME } from '../utils/queries';
 
-  const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
-    variables: { username: userParam },
-  });
+// import Auth from '../utils/auth';
 
-  const user = data?.me || data?.user || {};
-  // navigate to personal profile page if username is yours
-  if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
-    return <Navigate to="/me" />;
-  }
+// const Profile = () => {
+//   const { username: userParam } = useParams();
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+//   const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
+//     variables: { username: userParam },
+//   });
 
-  if (!user?.username) {
-    return (
-      <h4>
-        You need to be logged in to see this. Use the navigation links above to
-        sign up or log in!
-      </h4>
-    );
-  }
+//   const user = data?.me || data?.user || {};
+//   // navigate to personal profile page if username is yours
+//   if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
+//     return <Navigate to="/me" />;
+//   }
 
-  return (
-    <div>
-      <div className="flex-row justify-center mb-3">
-        <h2 className="col-12 col-md-10 bg-dark text-light p-3 mb-5">
-          Viewing {userParam ? `${user.username}'s` : 'your'} profile.
-        </h2>
+//   if (loading) {
+//     return <div>Loading...</div>;
+//   }
 
-        <div className="col-12 col-md-10 mb-5">
-          <Checklist
-          />
-        </div>
-        {!userParam && (
-          <div
-            className="col-12 col-md-10 mb-3 p-3"
-            style={{ border: '1px dotted #1a1a1a' }}
-          >
-            <Note />
-          </div>
-        )}
-      </div>
-    </div>
-  );
-};
+//   if (!user?.username) {
+//     return (
+//       <h4>
+//         You need to be logged in to see this. Use the navigation links above to
+//         sign up or log in!
+//       </h4>
+//     );
+//   }
 
-export default Profile;
+//   return (
+//     <div>
+//       <div className="flex-row justify-center mb-3">
+//         <h2 className="col-12 col-md-10 bg-dark text-light p-3 mb-5">
+//           Viewing {userParam ? `${user.username}'s` : 'your'} profile.
+//         </h2>
+
+//         <div className="col-12 col-md-10 mb-5">
+//           <Checklist
+//           />
+//         </div>
+//         {!userParam && (
+//           <div
+//             className="col-12 col-md-10 mb-3 p-3"
+//             style={{ border: '1px dotted #1a1a1a' }}
+//           >
+//             <Note />
+//           </div>
+//         )}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Profile;
