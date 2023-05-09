9caf40d50f38348269ee9f9146be2cf05c903d3f
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-05-09 16:33:48 -0400

    Add Link to="/" as={Link}, etc. to navigation links; Commented out other code (just in case)

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import '../App.css';
 
-export default function Header({ currentPage, handlePageChange }) {
+import { Link } from 'react-router-dom';
+import Auth from '../utils/auth';
+
+export default function Header() {
   return (
     <header className="header">
       <h1>Doggy Dojo</h1>
@@ -11,35 +14,70 @@ export default function Header({ currentPage, handlePageChange }) {
         role="group"
         aria-label="Outlined Buttons"
       >
-        <button
-          type="button"
-          className="navButtons"
-          onClick={() => handlePageChange('Homepage')}
-        >
-          Homepage
-        </button>
-        <button
-          type="button"
-          className="navButtons"
-          onClick={() => handlePageChange('Login')}
-        >
-          Login
-        </button>
-        <button
-          type="button"
-          className="navButtons"
-          onClick={() => handlePageChange('Signup')}
-        >
-          Signup
-        </button>
-        <button
-          type="button"
-          className="navButtons"
-          onClick={() => handlePageChange('Profile')}
-        >
-          Profile
-        </button>
+        <Link to="/" as={Link}>
+          <button type="button" className="navButtons">
+            Homepage
+          </button>
+        </Link>
+        <Link to="/me" as={Link}>
+          <button type="button" className="navButtons">
+            Profile
+          </button>
+        </Link>
+        <Link to="login" as={Link}>
+          <button type="button" className="navButtons">
+            Login
+          </button>
+        </Link>
+        <Link to="/signup" as={Link}>
+          <button type="button" className="navButtons">
+            Sign Up
+          </button>
+        </Link>
       </nav>
     </header>
   );
 }
+
+// export default function Header({ currentPage, handlePageChange }) {
+//   return (
+//     <header className="header">
+//       <h1>Doggy Dojo</h1>
+//       <h2>Helping Your Dog Reach Their Pet-tential!</h2>
+//       <nav
+//         className="button-container"
+//         role="group"
+//         aria-label="Outlined Buttons"
+//       >
+//         <button
+//           type="button"
+//           className="navButtons"
+//           onClick={() => handlePageChange('Homepage')}
+//         >
+//           Homepage
+//         </button>
+//         <button
+//           type="button"
+//           className="navButtons"
+//           onClick={() => handlePageChange('Login')}
+//         >
+//           Login
+//         </button>
+//         <button
+//           type="button"
+//           className="navButtons"
+//           onClick={() => handlePageChange('Signup')}
+//         >
+//           Signup
+//         </button>
+//         <button
+//           type="button"
+//           className="navButtons"
+//           onClick={() => handlePageChange('Profile')}
+//         >
+//           Profile
+//         </button>
+//       </nav>
+//     </header>
+//   );
+// }
