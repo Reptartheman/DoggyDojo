d5501f20b3c41f9643bb223c5e113ba682a067e4
Reptartheman <117610334+Reptartheman@users.noreply.github.com>
2023-05-10 20:02:05 -0400

    more CSS changes. Added dog bone

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,30 +1,17 @@
 import React, { useState } from "react";
 import "../App.css";
+import Bone from "../assets/bone.png"
 
 export default function Homepage() {
   return (
     <>
     <div className="gridContainer">
     <section className="gridItem1">
-      <h1>Welcome to Doggy Dojo!</h1>
+      <div className="headingImg">
+    <img src={Bone} className="gridImg"></img>
+      <h1>Welcome to Doggy Dojo!</h1>   
+      </div>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Eget felis eget nunc
-        lobortis mattis aliquam faucibus purus in. Ultrices eros in cursus
-        turpis. Tristique et egestas quis ipsum suspendisse. Lobortis feugiat
-        vivamus at augue eget arcu dictum varius. Morbi non arcu risus quis
-        varius quam quisque. Tortor pretium viverra suspendisse potenti nullam
-        ac tortor. Consectetur purus ut faucibus pulvinar elementum integer
-        enim. Habitasse platea dictumst vestibulum rhoncus est pellentesque.
-        Iaculis nunc sed augue lacus viverra vitae congue. Consequat nisl vel
-        pretium lectus quam id. At elementum eu facilisis sed. Ut ornare lectus
-        sit amet est placerat in. Hendrerit gravida rutrum quisque non tellus
-        orci.
-      </p>
-      </section>
-      <aside className="gridItem2">
-        <h3>The Latest Doggy headlines!</h3>
-        <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget felis
           eget nunc lobortis mattis aliquam faucibus purus in. Ultrices eros in
@@ -37,7 +24,45 @@ export default function Homepage() {
           Consequat nisl vel pretium lectus quam id. At elementum eu facilisis
           sed. Ut ornare lectus sit amet est placerat in. Hendrerit gravida
           rutrum quisque non tellus orci.
-        </p>
+        </p> 
+          <br />
+        <p>
+          Tortor pretium viverra suspendisse potenti
+          nullam ac tortor. Consectetur purus ut faucibus pulvinar elementum
+          integer enim. Habitasse platea dictumst vestibulum rhoncus est
+          pellentesque. Iaculis nunc sed augue lacus viverra vitae congue.
+          Consequat nisl vel pretium lectus quam id. At elementum eu facilisis
+          sed. Ut ornare lectus sit amet est placerat in. Hendrerit gravida
+          rutrum quisque non tellus orci.
+          Tortor pretium viverra suspendisse potenti
+          nullam ac tortor. Consectetur purus ut faucibus pulvinar elementum
+          integer enim. Habitasse platea dictumst vestibulum rhoncus est
+          pellentesque. Iaculis nunc sed augue lacus viverra vitae congue.
+          Consequat nisl vel pretium lectus quam id. At elementum eu facilisis
+          sed. Ut ornare lectus sit amet est placerat in. Hendrerit gravida
+          rutrum quisque non tellus orci.
+          </p>
+      </section>
+      <aside className="gridItem2">
+        <h3>The Latest Doggy headlines!</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget felis
+          eget nunc lobortis mattis aliquam faucibus purus in. Ultrices eros in
+          cursus turpis. Tristique et egestas quis ipsum suspendisse. Lobortis
+          feugiat vivamus at augue eget arcu dictum varius. Morbi non arcu risus
+          quis varius quam quisque.
+        </p> 
+          <br />
+        <p>
+          Tortor pretium viverra suspendisse potenti
+          nullam ac tortor. Consectetur purus ut faucibus pulvinar elementum
+          integer enim. Habitasse platea dictumst vestibulum rhoncus est
+          pellentesque. Iaculis nunc sed augue lacus viverra vitae congue.
+          Consequat nisl vel pretium lectus quam id. At elementum eu facilisis
+          sed. Ut ornare lectus sit amet est placerat in. Hendrerit gravida
+          rutrum quisque non tellus orci.
+          </p>
       </aside>
       </div>
     </>
